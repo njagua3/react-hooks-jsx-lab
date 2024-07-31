89dcdbc6899d3f6a34959d909d443d97bd70f9f1
njagua <njagua.gma@gmail.com>
2024-07-31 18:43:25 +0300

    add

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,18 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>
+        Hello! I am passionate about creating amazing web experiences. I enjoy
+        working with modern technologies and solving complex problems through
+        coding. When I'm not developing, you can find me exploring new places or
+        reading about the latest tech trends.
+      </p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
